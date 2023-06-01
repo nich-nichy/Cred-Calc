c10a2afac37ff6c61431f2f968b3d33439e36cee
Nishath <78273608+nich-nichy@users.noreply.github.com>
2023-06-01 04:18:28 +0000

    Update header.jsx

--- components/starter/header.jsx
+++ components/starter/header.jsx
@@ -1,47 +1,77 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import Cgpa from '../pages/cgpa.jsx'
-import Gpa from '../pages/gpa.jsx'
-
+import React from "react";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Cgpa from "../pages/cgpa.jsx";
+import Gpa from "../pages/gpa.jsx";
 
 export default function Header() {
   const handleCard1Press = () => {
-    return <Stack.Screen name="Cgpa" component={Cgpa} />
+    return <Stack.Screen name="Cgpa" component={Cgpa} />;
   };
   const handleCard2Press = () => {
-        return <Stack.Screen name="Gpa" component={Gpa} />
+    return <Stack.Screen name="Gpa" component={Gpa} />;
   };
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-         <TouchableOpacity style={styles.card1} onPress={handleCard1Press}>
-        <Image style={styles.cardImage} source={require('../../assets/')} />
-        <Text style={styles.cardTitle}>Card 1</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.card2} onPress={handleCard2Press}>
-        <Image style={styles.cardImage} source={{ uri: 'https://via.placeholder.com/350x150' }} />
-        <Text style={styles.cardTitle}>Card 2</Text>
-      </TouchableOpacity>
+    <NavigationContainer style={styles.container}>
+      {/* App starter */}
+      <View>
+        <Text style={styles.title}>CGPA and GPA Calculator</Text>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/Design-NVG.jpg")}
+        />
       </View>
-      </NavigationContainer>
+      {/* Card Wrapper */}
+      <View style={styles.cardHolder}>
+        <TouchableOpacity style={styles.card1} onPress={handleCard1Press}>
+          <Image
+            style={styles.cardImage}
+            source={require("../../assets/Design-NVG.jpg")}
+          />
+          <Text style={styles.cardTitle}>Card 1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.card2} onPress={handleCard2Press}>
+          <Image
+            style={styles.cardImage}
+            source={require("../../assets/Design-NVG.jpg")}
+          />
+          <Text style={styles.cardTitle}>Card 2</Text>
+        </TouchableOpacity>
+      </View>
+      {/* Loader */}
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    top: 0,
+  container: {},
+  title: {
+    color: "#fff",
+    margin: 20,
+    marginTop: 40,
+    fontSize: 30,
+    fontWeight: 700,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    borderRadius: "50%",
+    display: "relative",
+    alignSelf: "center",
+  },
+  cardHolder: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
+    justifyContent: "space-evenly",
+    display: "-webkit-inline-box",
+    marginTop: "20px",
   },
   card1: {
-    width: '45%',
+    width: 200,
     height: 200,
-    backgroundColor: '#fff',
+    margin: 10,
+    backgroundColor: "#fff",
     borderRadius: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -49,15 +79,16 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   card2: {
-    width: '45%',
+    width: 200,
     height: 200,
-    backgroundColor: '#fff',
+    margin: 10,
+    backgroundColor: "#fff",
     borderRadius: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -65,18 +96,18 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   cardImage: {
-    width: '100%',
-    height: '70%',
-    resizeMode: 'cover',
+    width: "100%",
+    height: "70%",
+    resizeMode: "cover",
     borderRadius: 10,
   },
   cardTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 10,
   },
 });
